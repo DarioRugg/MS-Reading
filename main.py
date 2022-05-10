e6b0b4531afe0e8b19c20d72aea7c3eebd1d69b2
DarioRugg <dario.ruggeri.ai@gmail.com>
2022-05-10 10:07:27 +0200

    working on .mzML files

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import raw2mzml
 import pymzml
 import matplotlib.pyplot as plt
 import numpy as np
 
-raw2mzml.convert_single_file()
-
-mzml_file = 'data/TQ8_201023SJ01_0103.raw'
+mzml_file = 'data/TQ8_201023SJ01_0103.mzML'
 
 msrun = pymzml.run.Reader(mzml_file)
+
