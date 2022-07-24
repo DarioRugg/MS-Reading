6b38e8bc4bf130a121f5a8892fcb8d33037c7f5f
DarioRugg <dario.ruggeri.ai@gmail.com>
2022-07-24 17:06:40 +0200

    last commit

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:latest
-
-COPY ./requirements.txt .
-
-RUN apt-get update && apt-get upgrade -y
-RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pymzml
+
 file = "data/TQ8_201023SJ01_0103.mzML"
 run = pymzml.run.Reader(
-        file,
-        MS1_Precision=5e-6,
-        MSn_Precision=20e-6
-    )
+    file
+)
 
-cumulative_spec = pymzml.spec.Spectrum(measured_precision = 20e-6)
+cumulative_spec = pymzml.spec.Spectrum(measured_precision=5e-1)
 
-for spec in run:
+for spec in list(run)[:50]:
     cumulative_spec += spec
 
+print(len(cumulative_spec.peaks("reprofiled")))
+print(run.MS_precisions)
 
-p = pymzml.plot.Factory()
-p.new_plot()
-p.add(cumulative_spec.peaks("reprofiled"), color=(0, 0, 0), style="sticks", name="peaks")
-p.save(filename="./img/comulative_spectrogram.html")
+# p = pymzml.plot.Factory()
+# p.new_plot()
+# p.add(cumulative_spec.peaks("reprofiled"), color=(0, 0, 0), style="sticks", name="peaks")
+# p.save(filename="./img/cumulative_spectrogram.html")
